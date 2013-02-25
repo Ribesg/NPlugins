51eadc36cdccce4d38b8dc78228fd909dc6a2e05
Ribesg <Ribesg@yahoo.fr>
2013-02-25 21:39:03 +0100

    Remove Eclipse things from Git repo

--- NPunisher/src/com/github/ribesg/npunisher/NPunisher.java
+++ NPunisher/src/com/github/ribesg/npunisher/NPunisher.java
@@ -4,63 +4,59 @@ import lombok.Getter;
 
 import org.bukkit.Bukkit;
 
-import com.github.ribesg.ncore.NCore;
-import com.github.ribesg.ncore.nodes.cuboid.CuboidNode;
-import com.github.ribesg.ncore.nodes.punisher.PunisherNode;
-
 public class NPunisher extends PunisherNode {
 
-    // Core plugin
-    public static final String NCORE           = "NCore";
-    @Getter public NCore       core;
+	// Core plugin
+	public static final String	NCORE			= "NCore";
+	@Getter public NCore		core;
 
-    // Useful Nodes
-    public static final String NCUBOID         = "NCuboid";
-    @Getter public CuboidNode  cuboidNode;
+	// Useful Nodes
+	public static final String	NCUBOID			= "NCuboid";
+	@Getter public CuboidNode	cuboidNode;
 
-    // Set to true by afterEnable() call
-    // Prevent multiple calls to afterEnable
-    private boolean            loadingComplete = false;
+	// Set to true by afterEnable() call
+	// Prevent multiple calls to afterEnable
+	private boolean				loadingComplete	= false;
 
-    @Override
-    public void onEnable() {
-        if (!Bukkit.getPluginManager().isPluginEnabled(NCORE)) {
-            // TODO
-        } else {
-            core = (NCore) Bukkit.getPluginManager().getPlugin(NCORE);
-            // TODO
-            afterEnable();
-        }
-    }
+	@Override
+	public void onEnable() {
+		if (!Bukkit.getPluginManager().isPluginEnabled(NCORE)) {
+			// TODO
+		} else {
+			core = (NCore) Bukkit.getPluginManager().getPlugin(NCORE);
+			// TODO
+			afterEnable();
+		}
+	}
 
-    public void afterEnable() {
-        if (!loadingComplete) {
-            loadingComplete = true;
-            Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+	public void afterEnable() {
+		if (!loadingComplete) {
+			loadingComplete = true;
+			Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 
-                @Override
-                public void run() {
-                    // Interact with other Nodes here
-                    if (!Bukkit.getPluginManager().isPluginEnabled(NCUBOID)) {
-                        // TODO
-                    } else {
-                        cuboidNode = (CuboidNode) Bukkit.getPluginManager().getPlugin(NCUBOID);
-                        // TODO
-                        afterEnable();
-                    }
-                }
-            });
-        }
-    }
+				@Override
+				public void run() {
+					// Interact with other Nodes here
+					if (!Bukkit.getPluginManager().isPluginEnabled(NCUBOID)) {
+						// TODO
+					} else {
+						cuboidNode = (CuboidNode) Bukkit.getPluginManager().getPlugin(NCUBOID);
+						// TODO
+						afterEnable();
+					}
+				}
+			});
+		}
+	}
 
-    @Override
-    public void onDisable() {
+	@Override
+	public void onDisable() {
 
-    }
+	}
 
-    public void setCore(final NCore core) {
-        this.core = core;
-        core.setPunisherNode(this);
-    }
+	public void setCore(final NCore core) {
+		this.core = core;
+		core.setPunisherNode(this);
+	}
 
 }
