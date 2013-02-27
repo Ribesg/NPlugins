c0b7e6a3e0074092c8119c0e44adedf7817beac2
Ribesg <ribesg@yahoo.fr>
2013-02-27 10:02:59 +0100

    Added Localization handling in all plugins

---
+++ NTheEndAgain/src/fr/ribesg/bukkit/ntheendagain/lang/Message.java
@@ -0,0 +1,73 @@
+package fr.ribesg.bukkit.ntheendagain.lang;
+
+import lombok.Getter;
+
+import fr.ribesg.bukkit.ntheendagain.lang.Messages.MessageId;
+
+public class Message {
+    @Getter private final MessageId id;
+    @Getter private final String    defaultMessage;
+    @Getter private final String    configMessage;
+    @Getter private final String[]  awaitedArgs;
+
+    public Message(final MessageId id, final String defaultMessage, final String[] awaitedArgs) {
+        this.id = id;
+        this.defaultMessage = defaultMessage;
+        this.awaitedArgs = awaitedArgs;
+        configMessage = null;
+    }
+
+    public Message(final MessageId id, final String defaultMessage, final String[] awaitedArgs, final String configMessage) {
+        this.id = id;
+        this.defaultMessage = defaultMessage;
+        this.awaitedArgs = awaitedArgs;
+        this.configMessage = configMessage;
+    }
+
+    public String getAwaitedArgsString() {
+        if (awaitedArgs == null) {
+            return "none";
+        } else {
+            final StringBuilder s = new StringBuilder();
+            for (final String arg : awaitedArgs) {
+                s.append(arg);
+                s.append(" ; ");
+            }
+            return s.toString().substring(0, s.length() - 3);
+        }
+    }
+
+    public int getAwaitedArgsNb() {
+        if (awaitedArgs == null) {
+            return 0;
+        } else {
+            return awaitedArgs.length;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (id == null ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Message other = (Message) obj;
+        if (id != other.id) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ NTheEndAgain/src/fr/ribesg/bukkit/ntheendagain/lang/Messages.java
@@ -0,0 +1,152 @@
+package fr.ribesg.bukkit.ntheendagain.lang;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.Getter;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class Messages {
+
+    public enum MessageId {
+
+        // General plugin messages
+        errorWhileLoadingConfiguration,
+
+        // General deny response
+        noPermissionForCommand,
+        cmdOnlyAvailableForPlayers,
+
+        // Command - RELOAD
+        cmdReloadConfig,
+        cmdReloadMessages,
+
+    }
+
+    public static final String                         LINE_SEPARATOR = "%%";
+    public static final String                         MESSAGE_HEADER = "§0§l[§c§lN§6§lChat§0§l] §f";
+    public static final Charset                        CHARSET        = Charset.defaultCharset();
+
+    @Getter private static EnumMap<MessageId, Message> messagesMap;                                  // Id ; Message
+
+    public static void loadConfig(final Path pathMessages) throws IOException {
+        messagesMap = getDefaultConfig();
+        if (!Files.exists(pathMessages)) {
+            newConfig(pathMessages);
+        } else {
+            final YamlConfiguration cMessages = new YamlConfiguration();
+            try (BufferedReader reader = Files.newBufferedReader(pathMessages, CHARSET)) {
+                final StringBuilder s = new StringBuilder();
+                while (reader.ready()) {
+                    s.append(reader.readLine() + '\n');
+                }
+                cMessages.loadFromString(s.toString());
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+            for (final String idString : cMessages.getKeys(false)) {
+                try {
+                    final MessageId id = MessageId.valueOf(idString);
+                    final Message def = messagesMap.get(id);
+                    messagesMap.put(id, new Message(id, def.getDefaultMessage(), def.getAwaitedArgs(), cMessages.getString(idString, def.getDefaultMessage())));
+                } catch (final IllegalArgumentException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+            }
+            overwriteConfig(pathMessages);
+        }
+    }
+
+    public static EnumMap<MessageId, Message> getDefaultConfig() {
+        final Set<Message> newMessages = new HashSet<Message>();
+
+        // General plugin messages
+        newMessages.add(new Message(MessageId.errorWhileLoadingConfiguration, "&cError while loading config file %filename%", new String[] { "%filename%" }, null));
+
+        // General deny response
+        newMessages.add(new Message(MessageId.noPermissionForCommand, "&cYou do not have the permission to use that command", null, null));
+        newMessages.add(new Message(MessageId.cmdOnlyAvailableForPlayers, "&cThis command is only available in game", null, null));
+
+        // Command - RELOAD
+        newMessages.add(new Message(MessageId.cmdReloadConfig, "&aConfig reloaded !", null, null));
+        newMessages.add(new Message(MessageId.cmdReloadMessages, "&aMessages reloaded !", null, null));
+
+        final EnumMap<MessageId, Message> map = new EnumMap<MessageId, Message>(MessageId.class);
+        for (final Message m : newMessages) {
+            map.put(m.getId(), m);
+        }
+        return map;
+    }
+
+    private static void newConfig(final Path pathMessages) throws IOException {
+        writeConfig(pathMessages, false);
+    }
+
+    private static void overwriteConfig(final Path pathMessages) throws IOException {
+        writeConfig(pathMessages, true);
+    }
+
+    private static void writeConfig(final Path pathMessages, final boolean overwrite) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(pathMessages, CHARSET, overwrite ? StandardOpenOption.TRUNCATE_EXISTING : StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE)) {
+            final StringBuilder content = new StringBuilder();
+            content.append("################################################################################\n");
+            content.append("# List of NChat messages. You're free to change text/colors/language here.     #\n");
+            content.append("# Supports both '§' and '&' characters for colors.                      Ribesg #\n");
+            content.append("################################################################################\n\n");
+            for (final Message m : getMessagesMap().values()) {
+                content.append("# Default value    : " + m.getDefaultMessage() + '\n');
+                content.append("# Awaited arguments: " + m.getAwaitedArgsString() + '\n');
+                content.append(m.getId().name() + ": \"" + (m.getConfigMessage() != null ? m.getConfigMessage() : m.getDefaultMessage()) + "\"\n\n");
+            }
+            writer.write(content.toString());
+        }
+    }
+
+    // Args have to be in the same order than in Message.getAwaitedArgs()
+    public static String[] get(final MessageId id, final String... args) {
+        try {
+            final Message m = getMessagesMap().get(id);
+            if (args != null && args.length != m.getAwaitedArgsNb() || args == null && m.getAwaitedArgsNb() > 0) {
+                throw new IllegalArgumentException("Call to Messages.get(id,args...) with wrong number of args : " + (args == null ? 0 : args.length) + " (awaited : " + m.getAwaitedArgsNb() + ")");
+            }
+            String res = m.getConfigMessage() == null ? m.getDefaultMessage() : m.getConfigMessage();
+            // Replacing args by there values
+            for (int i = 0; i < m.getAwaitedArgsNb(); i++) {
+                res = res.replace(m.getAwaitedArgs()[i], args[i]);
+            }
+            // Adding Header, colors
+            final String[] resSplit = res.concat(LINE_SEPARATOR).split(LINE_SEPARATOR);
+            for (int i = 0; i < resSplit.length; i++) {
+                resSplit[i] = MESSAGE_HEADER + ChatColor.translateAlternateColorCodes('&', resSplit[i]);
+            }
+            return resSplit;
+        } catch (final IllegalArgumentException e) {
+            e.printStackTrace();
+            return new String[] { MESSAGE_HEADER + ChatColor.RED + "Something gone wrong, see console" };
+        }
+    }
+
+    public static String merge(final String[] strings) {
+        if (strings == null || strings.length < 1) {
+            return null;
+        } else {
+            final StringBuilder res = new StringBuilder(strings[0]);
+            for (int i = 1; i < strings.length; i++) {
+                res.append(LINE_SEPARATOR);
+                res.append(strings[i]);
+            }
+            return res.toString();
+        }
+    }
+}
