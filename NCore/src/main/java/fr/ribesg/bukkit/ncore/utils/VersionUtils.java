9602e151f9e6b59746ee56f913e0828234ec8037
Ribesg <ribesg@yahoo.fr>
2013-12-04 14:23:38 +0100

    [NCore] Add VersionUtils

---
+++ NCore/src/main/java/fr/ribesg/bukkit/ncore/utils/VersionUtils.java
@@ -0,0 +1,30 @@
+/***************************************************************************
+ * Project file:    NPlugins - NCore - VersionUtils.java                   *
+ * Full Class name: fr.ribesg.bukkit.ncore.utils.VersionUtils              *
+ *                                                                         *
+ *                Copyright (c) 2013 Ribesg - www.ribesg.fr                *
+ *   This file is under GPLv3 -> http://www.gnu.org/licenses/gpl-3.0.txt   *
+ *    Please contact me at ribesg[at]yahoo.fr if you improve this file!    *
+ ***************************************************************************/
+
+package fr.ribesg.bukkit.ncore.utils;
+
+/**
+ * Utility class to play with Plugin versions
+ *
+ * @author Ribesg
+ */
+public class VersionUtils {
+
+	private static final String SNAPSHOT_SUFFIX = "-SNAPSHOT";
+
+	/**
+	 * Check if a version represents a Snapshot version or not.
+	 *
+	 * @return true if the provided String represents a Snapshot version,
+	 *         false otherwise
+	 */
+	public static boolean isSnapshot(final String versionString) {
+		return versionString.endsWith(SNAPSHOT_SUFFIX);
+	}
+}
