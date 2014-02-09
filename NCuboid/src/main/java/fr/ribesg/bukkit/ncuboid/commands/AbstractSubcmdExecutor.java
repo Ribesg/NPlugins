052c8d769a70e08e60f3d186f5d22ee2fd59054f
Ribesg <Ribesg@yahoo.fr>
2014-02-09 10:09:27 +0100

    [NCuboid] Allow multiline usage

--- NCuboid/src/main/java/fr/ribesg/bukkit/ncuboid/commands/AbstractSubcmdExecutor.java
+++ NCuboid/src/main/java/fr/ribesg/bukkit/ncuboid/commands/AbstractSubcmdExecutor.java
@@ -16,8 +16,8 @@ import java.util.Arrays;
 
 public abstract class AbstractSubcmdExecutor {
 
-	private final NCuboid plugin;
-	private       String  usage;
+	private final NCuboid  plugin;
+	private       String[] usage;
 
 	public AbstractSubcmdExecutor(final NCuboid instance) {
 		this.plugin = instance;
@@ -28,14 +28,16 @@ public abstract class AbstractSubcmdExecutor {
 	}
 
 	/** Should be called in sub-classes' constructors */
-	protected void setUsage(final String usage) {
+	protected void setUsage(final String... usage) {
 		this.usage = usage;
 	}
 
 	public boolean execute(final CommandSender sender, final String[] superCommandArgs) {
 		final boolean result = exec(sender, Arrays.copyOfRange(superCommandArgs, 1, superCommandArgs.length));
 		if (!result) {
-			sender.sendMessage(getPlugin().getMessages().getMessageHeader() + this.usage);
+			for (final String line : this.usage) {
+				sender.sendMessage(getPlugin().getMessages().getMessageHeader() + line);
+			}
 		}
 		return true;
 	}
